781d141c59b7c0ea67f649d51dcdf0427ada385a
nishantkathrotiya <mojonly813@gmil.com>
2025-09-15 17:58:53 +0530

    Fix : Queue TIE break on same vote

--- app/api/streams/next/route.ts
+++ app/api/streams/next/route.ts
@@ -3,61 +3,62 @@ import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const session = await getServerSession();
-    // TODO: You can get rid of the db call here 
-    const user = await prismaClient.user.findFirst({
-       where: {
-           email: session?.user?.email ?? ""
-       }
-   });
+  const session = await getServerSession();
+  // TODO: You can get rid of the db call here
+  const user = await prismaClient.user.findFirst({
+    where: {
+      email: session?.user?.email ?? "",
+    },
+  });
 
-   if (!user) {
-       return NextResponse.json({
-           message: "Unauthenticated"
-       }, {
-           status: 403
-       })
-   }
-   console.log("before first call");
+  if (!user) {
+    return NextResponse.json(
+      {
+        message: "Unauthenticated",
+      },
+      {
+        status: 403,
+      }
+    );
+  }
+  console.log("before first call");
 
-   const mostUpvotedStream = await prismaClient.stream.findFirst({
-        where: {
-            userId: user.id,
-            played: false
-        },
-        orderBy: {
-            upvotes: {
-                _count: 'desc'
-            }
-        }
-   });
-   console.log("after first call");
-   console.log(mostUpvotedStream?.id )
- 
-   await Promise.all([prismaClient.currentStream.upsert({
-        where: {
-            userId: user.id
-        },
-        update: {
-            userId: user.id,
-            streamId: mostUpvotedStream?.id 
-        },
-        create: {
-            userId: user.id,
-            streamId: mostUpvotedStream?.id
-        }
-    }), prismaClient.stream.update({
-        where: {
-            id: mostUpvotedStream?.id ?? ""
-        },
-        data: {
-            played: true,
-            playedTs: new Date()
-        }
-   })])
+  const mostUpvotedStream = await prismaClient.stream.findFirst({
+    where: {
+      userId: user.id,
+      played: false,
+    },
+    orderBy: [{ upvotes: "desc" }, { createAt: "asc" }],
+  });
+  console.log("after first call");
+  console.log(mostUpvotedStream?.id);
 
-   return NextResponse.json({
-    stream: mostUpvotedStream
-   })
-   
+  await Promise.all([
+    prismaClient.currentStream.upsert({
+      where: {
+        userId: user.id,
+      },
+      update: {
+        userId: user.id,
+        streamId: mostUpvotedStream?.id,
+      },
+      create: {
+        userId: user.id,
+        streamId: mostUpvotedStream?.id,
+      },
+    }),
+    prismaClient.stream.update({
+      where: {
+        id: mostUpvotedStream?.id ?? "",
+      },
+      data: {
+        played: true,
+        playedTs: new Date(),
+      },
+    }),
+  ]);
+
+  return NextResponse.json({
+    stream: mostUpvotedStream,
+  });
 }
--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,8 +1,10 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export const YT_REGEX = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
+// export const YT_REGEX = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/;
+export const YT_REGEX =
+  /(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtube\.com\/(?:.*[?&]v=|(?:v|embed)\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})(?:[?&\s][^ ]*)?/;
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
